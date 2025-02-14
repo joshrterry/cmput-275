e8fa02b0a7c8d1495757ae2ea0fe71e615c71457
Joshua Terry <joshua.terry1@icloud.com>
2025-02-14 15:02:05 -0700

    more changes

--- a2/q1/luhns.c
+++ a2/q1/luhns.c
@@ -3,12 +3,12 @@
 
 int main() {
     int sum = 0;
-    int pos = 0; // Position from right (1-based)
+    int pos = 0; // position from right (1-based)
     int check_digit = -1;
     char ch;
     
     while ((ch = getchar()) >= '0' && ch <= '9') {
-        int digit = ch - '0';
+        int digit = ch - '0'; // convert from char to int
         
         if (check_digit == -1) {
             check_digit = digit; // Store the last digit as check digit
